1fb2e6e13ff57ea6d6f744be97e2e7e2742df32f
Ben Orchard <thefirstmuffinman@gmail.com>
2021-12-02 22:02:46 +0000

    finish initial hex bytestring tests

--- test/BytePatch/Pretty/HexByteStringSpec.hs
+++ test/BytePatch/Pretty/HexByteStringSpec.hs
@@ -14,6 +14,9 @@ spec = do
       parse "00" `shouldBe` Just (bs [0x00])
       parse "FF" `shouldBe` Just (bs [0xFF])
       parse "1234" `shouldBe` Just (bs [0x12, 0x34])
+      parse "01 9A FE" `shouldBe` Just (bs [0x01, 0x9A, 0xFE])
+      parse "FFFFFFFF" `shouldBe` Just (BS.replicate 4 0xFF)
+      parse "12 34    AB CD" `shouldBe` Just (bs [0x12, 0x34, 0xAB, 0xCD])
     it "fails to parse invalid hex bytestrings" $ do
       parse "-00" `shouldBe` Nothing
       parse "FG" `shouldBe` Nothing
