47765b0e0e6a4a728b700b9c3d3eb9e3ef1f5f83
Ben Orchard <thefirstmuffinman@gmail.com>
2023-11-15 17:38:26 +0000

    update Nix flake

--- flake.nix
+++ flake.nix
@@ -4,21 +4,19 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, config, ... }: {
-        packages.default = self'.packages.bytepatch;
-        haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
-        haskellProjects.default = {
-          devShell = {
-            tools = hp: {
-              ghcid = null; # broken on GHC 9.6? old fsnotify
-              hlint = null; # broken on GHC 9.6? old
-              haskell-language-server = null; # TAKES AGES TO BUILD FFS
-            };
-          };
+        packages.default = self'.packages.ghc96-bytepatch;
+        devShells.default = self'.devShells.ghc96;
+        haskellProjects.ghc96-base = import ./haskell-flake-ghc96.nix pkgs;
+        haskellProjects.ghc96 = {
+          # 2023-11-14: GHC 9.6 base package set is borked
+          # PR: https://github.com/NixOS/nixpkgs/pull/267477
+          basePackages = config.haskellProjects.ghc96-base.outputs.finalPackages;
+          devShell.mkShellArgs.name = "ghc96-bytepatch";
         };
       };
     };
--- haskell-flake-ghc96.nix
+++ haskell-flake-ghc96.nix
@@ -6,44 +6,21 @@ pkgs:
   devShell.enable = false;
   autoWire = [];
 
-  basePackages = pkgs.haskell.packages.ghc96;
-
-  packages = { #self: super: with pkgs.haskell.lib; {
-    # 2023-04-17 raehik: need hedgehog 1.2 for GHC 9.6
-    hedgehog.source = "1.2";
-    tasty-hedgehog.source = "1.4.0.1";
-
-    # 2023-04-17 raehik: warp: need new for GHC 9.6 (unix-2.8)
-    # also has 3 test failures. idk why. disabling
-    # also has friends that need swapping out. heck on earth
-    warp.source = "3.3.25";
-    recv.source = "0.1.0";
-    warp-tls.source = "3.3.6";
+  # 2023-11-14: GHC 9.6 base package set is borked
+  # PR: https://github.com/NixOS/nixpkgs/pull/267477
+  basePackages = pkgs.haskell.packages.ghc96.override {
+    overrides = self: super: {
+      fgl = self.fgl_5_8_2_0;
+      th-desugar = self.th-desugar_1_16;
+    };
   };
 
-  settings = {
-
-    hourglass = {
-      # 2023-04-17 raehik: hourglass tests broken from GHC 9.2.5
-      # PR: https://github.com/vincenthz/hs-hourglass/pull/56
-      check = false;
-    };
-
-    bsb-http-chunked = {
-      # 2023-04-17 raehik: bsb-http-chunked: tests broken
-      # maybe problematic type wildcard usage...?
-      check = false;
-    };
-
-    doctest-exitcode-stdio = {
-      # 2023-04-26 raehik: weird bug. bad test.
-      jailbreak = true;
-    };
-
-    warp = {
-      check = false;
-    };
-
+  packages = {
+    # singletons-{th,base} only have 3.0 and 3.3 (GHC 9.8), need 3.2 for GHC 9.6
+    singletons-th.source   = "3.2";
+    singletons-base.source = "3.2";
+    # but now we have to re-override th-desugar bceause singletons-x 3.2 uses
+    # th-desugar 1.15...
+    th-desugar.source = "1.15";
   };
-
 }
